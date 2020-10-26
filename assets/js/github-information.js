9cb1d604c29000c3d13fad4654bada031a990394
KSheeds1 <katiesheedy@hotmail.com>
2020-10-26 12:43:00 +0000

    Render user information from GitHub and fix spelling errors.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,24 @@
-function fecthGitHubInformation(event) {
+function userInformationHTML(user) {
+    return ` 
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url} target="_blank>
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}"/>
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+
+
+
+function fetchGitHubInformation(event) {
 
     let username = $("#gh-username").val();
     if (!username) {
@@ -11,9 +31,9 @@ function fecthGitHubInformation(event) {
          </div>`);
     
     $.when(
-        $.getJSON(`https://api.hithub.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
-        function(repsonse) {
+        function(response) {
             let userData = response;
             $("#gh-user-data").html(userInformationHTML(userData));
         }, function(erroResponse) {
